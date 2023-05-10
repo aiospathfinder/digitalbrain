242f7e12cd402e48353bda86c94f26d77075a19b
Emmanuel Akanji <ea@afterflea.com>
2023-05-10 18:41:34 +0100

    changed the exposed ports

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-b^vkps*75g02y8vrlg-g&&07ce5#r4j$zk22wh&ho)v3)aj1$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = [
+    # '0.0.0.0'
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
