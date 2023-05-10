493a920c510cb974c71892bc87abe693ca4f3dfd
bedAF <bed@afterflea.com>
2023-05-10 16:12:42 +0100

    First iteration

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b^vkps*75g02y8vrlg-g&&07ce5#r4j$zk22wh&ho)v3)aj1$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   
+STATIC_URL = 'static/'     
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
