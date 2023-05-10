c6938d591594e9aa37264f00d12a374523c5fef1
CyberWarriorGuru <mrogolino976@outlook.com>
2023-05-10 12:31:45 -0700

    completeDjangoDigitalbrain0510

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -45,9 +45,9 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'digitalbrain.urls'
