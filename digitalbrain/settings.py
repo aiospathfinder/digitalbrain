da2b0c2ffafc95248f0e83d2a14eb2a70bf1b7b0
bedAF <bed@afterflea.com>
2023-05-10 22:07:11 +0100

    debugging

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-b^vkps*75g02y8vrlg-g&&07ce5#r4j$zk22wh&ho)v3)aj1$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+CSRF_TRUSTED_ORIGINS = ['https://aios-universal-brain-c4unnrrr4a-uc.a.run.app/', 'https://aios-universal-brain-c4unnrrr4a-uc.a.run.app/']
 ALLOWED_HOSTS = ["*"]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
