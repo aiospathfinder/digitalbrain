1183a8055c9ee5b2d0f9cd12a4197405a7037059
Emmanuel Akanji <ea@afterflea.com>
2023-05-11 16:45:11 +0100

    Fixed the docker image issue

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -117,8 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Secure proxy 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_TRUSTED_ORIGINS = ['https://*.run.app', 'https://*.0.0.0.0']
