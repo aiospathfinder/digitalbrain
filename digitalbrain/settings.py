46c8fd52f372b77b9bbe8ff805919ccb2556b080
bedAF <132270607+bedAF@users.noreply.github.com>
2023-05-11 11:58:30 +0100

    Pull to recent (#4)
    * turn off debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * Update settings.py
    
    * Update requirements.txt

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^vkps*75g02y8vrlg-g&&07ce5#r4j$zk22wh&ho)v3)aj1$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -115,9 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   
+STATIC_URL = 'static/'     
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
